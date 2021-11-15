5dca60c0413526fa9410e417cddcd485e0244da3
Deperius <jhonk1999@gmail.com>
2021-11-15 15:38:39 -0500

    migrations & postman test all ok

--- guarauthProject/urls.py
+++ guarauthProject/urls.py
@@ -1,11 +1,8 @@
-from django.contrib             import admin
-from django.urls                import path
-from rest_framework_simplejwt   import (TokenObtainPairView, TokenRefreshView)
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from guarauthApp                import views
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from guarauthApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
     path('verifyToken/', views.VerifyTokenView.as_view()),
